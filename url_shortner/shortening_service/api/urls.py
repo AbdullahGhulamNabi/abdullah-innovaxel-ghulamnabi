eaa7db5791644195354497ce74c2e8e30de005ef
AbdullahGhulamNabi <abdullahghulamnabi18@gmail.com>
2025-04-20 15:33:54 +0500

    updated urls and template
    the urls are now mapped to ones given in tasks, and index.html is updated to give the responses in each separate div below each form, rather than overwriting the same one

--- url_shortner/shortening_service/api/urls.py
+++ url_shortner/shortening_service/api/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', shortener_page, name='shortener_page'),
-    path('create/', CreateShortURL.as_view(), name='create_short_url'),  
-    path('retreive/<str:pk>/', RetrieveShortURL.as_view(), name='create_short_url'),  
+    path('shorten/', CreateShortURL.as_view(), name='create_short_url'),  
+    path('shorten/<str:pk>/', RetrieveShortURL.as_view(), name='create_short_url'),  
     path('<str:pk>', RedirectToOriginalURL.as_view(), name='redirect-url'),
 ]
